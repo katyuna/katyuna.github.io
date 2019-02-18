3e0cb62c558ce42cd2d9ffa48fcb58fab84a4fa1
ezarubina <edubovenko@gmail.com>
2019-02-18 18:56:40 +0700

    add 2 homework

---
+++ Practice1/scripts/script.js
@@ -0,0 +1,14 @@
+'use strict';
+
+//let x = 5; 
+//console.log(x++);
+
+//let y = 1; 
+////let x = y = 2; 
+//console.log(x); 
+//let x = [ ] + false - null + true;
+//let x=[ ] + 1 + 2;
+// console.log( "1"[0] );
+//console.log (2 && 1 && null && 0 && undefined );
+//console.log(x);
+
